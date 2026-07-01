ba2b918543c2775428f76a2d890784a3cb119785
ThiagoCorreaSilva <titi.c2006@gmail.com>
2026-07-01 20:42:34 -0300

    To implement SystemsManager need to change who handles the entities components container

--- headers/Components.hpp
+++ headers/Components.hpp
@@ -4,15 +4,15 @@
 
 namespace Components
 {
+    struct Render
+    {
+        Color color = PURPLE;
+    };
+
     struct Transform
     {
         Vector2 position = Vector2(0, 0);
         float size = 10.f;
         float rotation = 0.f;    
-    };
-
-    struct Render
-    {
-        Color color = PURPLE;
     };
 }
---
+++ headers/Systems/Render.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+class Render
+{
+public:
+    void RenderEntities()
+    {
+        
+    };
+};
