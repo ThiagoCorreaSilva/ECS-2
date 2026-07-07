d73b953842ae12b13f9cdd9988957bdc779d9c67
ThiagoCorreaSilva <titi.c2006@gmail.com>
2026-07-07 18:54:48 -0300

    Mouse now can select colors

--- headers/GameUI.hpp
+++ headers/GameUI.hpp
@@ -4,21 +4,27 @@
 #include <raylib.h>
 
 #include <array>
+#include <utility>
 #include <iostream>
 
-constexpr int squares = 3;
+constexpr int SQUARES_COUNT = 3;
 
 class GameUI
 {
 private:
-    std::array<Color, squares> colors;
+    std::array<std::pair<Rectangle, Color>, SQUARES_COUNT> squares; 
+    
     float size = 100;
     int selectedColor = -1;
+
+private:
+    void ChangeSelectedColor(const int& number);
 
 public:
     GameUI();
 
     void Update();
-    void Input();
     void AbilitiesUI();
+    void Input();
+    void SelectWithMouse();
 };
--- src/GameUI.cpp
+++ src/GameUI.cpp
@@ -3,26 +3,51 @@
 
 GameUI::GameUI()
 {
-    colors.at(0) = BLUE;
-    colors.at(1) = RED;
-    colors.at(2) = YELLOW;
+    for (int i = 0; i < SQUARES_COUNT; i++)
+    {
+        Rectangle rect {0, 0, size, size};
+        Color color;
+
+        switch (i)
+        {
+            case 0:
+                color = BLUE;
+                break;
+
+            case 1:
+                color = YELLOW;
+                break;
+
+            case 2:
+                color = RED;
+                break;
+        }
+        std::pair<Rectangle, Color> pair = {rect, color};
+        squares.at(i) = pair;
+    }
 }
+
+void GameUI::ChangeSelectedColor(const int& number)
+{
+    selectedColor = selectedColor == number ? -1 : number;
+};
 
 void GameUI::Update()
 {
     AbilitiesUI();
     Input();
+    SelectWithMouse();
 }
 
 void GameUI::AbilitiesUI()
 {
     static const int spacing = size / 2;
-    static const int totalWidth = (squares * size) + ((squares - 1) * spacing);
+    static const int totalWidth = (SQUARES_COUNT * size) + ((SQUARES_COUNT - 1) * spacing);
     
     static const float x = (GetScreenWidth() - totalWidth) / 1.1f; // DONT CHANGE THIS SHIT OR ALL OF THIS WILL BRAKE LOL
     static const float y = GetScreenHeight() - (GetScreenHeight() / 7.f);
 
-    for (int i = 0; i < squares; i++)
+    for (int i = 0; i < SQUARES_COUNT; i++)
     {
         float customSize = size;
         float offset_x = x - i * (customSize + spacing);
@@ -32,9 +57,15 @@ void GameUI::AbilitiesUI()
             customSize *= 1.3f;
         }
 
-        Rectangle rect {offset_x - (customSize / 2.f), y - (customSize / 2.f), customSize, customSize};
+        auto& rect = squares.at(i).first;
+        auto color = squares.at(i).second;
 
-        GuiDrawRectangle(rect, 3, BLACK, colors.at(i));
+        rect.x = offset_x - (customSize / 2.f);
+        rect.y = y - (customSize / 2.f);
+        rect.width = customSize;
+        rect.height = customSize;
+
+        GuiDrawRectangle(rect, 3, BLACK, color);
     }
 }
 
@@ -42,16 +73,27 @@ void GameUI::Input()
 {
     if (IsKeyReleased(KEY_ONE))
     {
-        selectedColor = selectedColor == 2 ? -1 : 2;
+        ChangeSelectedColor(2);
     }
 
     if (IsKeyReleased(KEY_TWO))
     {
-        selectedColor = selectedColor == 1 ? -1 : 1;
+        ChangeSelectedColor(1);
     }
 
     if (IsKeyReleased(KEY_THREE))
     {
-        selectedColor = selectedColor == 0 ? -1 : 0;
+        ChangeSelectedColor(0);
+    }
+}
+
+void GameUI::SelectWithMouse()
+{
+    for (int i = 0; i < SQUARES_COUNT; i++)
+    {
+        if (IsMouseButtonReleased(0) && CheckCollisionPointRec(GetMousePosition(), squares.at(i).first))
+        {
+            ChangeSelectedColor(i);
+        }
     }
 }
